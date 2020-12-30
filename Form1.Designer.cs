c66a55d6606595517852a9fb1f39e7fd10b26ce7
FilippoTurati55 <carlo.cattaneo4@gmail.com>
2020-12-30 18:43:33 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace FotoOrganizzatore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.immagine1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.immagine1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // immagine1
+            // 
+            this.immagine1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.immagine1.InitialImage = null;
+            this.immagine1.Location = new System.Drawing.Point(12, 31);
+            this.immagine1.Name = "immagine1";
+            this.immagine1.Size = new System.Drawing.Size(1577, 881);
+            this.immagine1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.immagine1.TabIndex = 0;
+            this.immagine1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 42F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2215, 1070);
+            this.Controls.Add(this.immagine1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.immagine1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox immagine1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FotoOrganizzatore
+{
+    public partial class Form1 : Form
+    {
+        Image i;
+        public Form1()
+        {
+            InitializeComponent();
+            immagine1.Image = leggiImmagineDaFile(@"c:\foto\2018\01 01\\WP_20180101_10_08_18_Rich.jpg");
+        }
+        public static Image leggiImmagineDaFile(string path)
+        {
+            Bitmap myImage = null;
+            PropertyItem propItem4 = null;
+            try
+            {
+                if (File.Exists(path))
+                {
+                    myImage = (Bitmap)System.Drawing.Image.FromFile(path);
+                    try
+                    {
+                        propItem4 = myImage.GetPropertyItem(0x112);
+                        switch (propItem4.Value[0])
+                        {
+                            case 0: break;
+                            case 6:
+                                myImage.RotateFlip(RotateFlipType.Rotate90FlipX);
+                                myImage.RotateFlip(RotateFlipType.Rotate180FlipY);
+                                break;
+                            case 8:
+                                break;
+                            case 3:
+                                myImage.RotateFlip(RotateFlipType.Rotate180FlipX);
+                                break;
+                            default:
+                                    break;
+                        }
+                    }
+                    catch { };
+                }
+            }
+            catch
+            {
+                myImage = null;
+                // VarGlo.tracciaEccezioni("leggiImmagineDaFile myImage = null" + path + "\r\n");
+            }
+            return myImage;
+        }
+    }
+}
